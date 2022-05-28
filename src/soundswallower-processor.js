26e9ef0c4107a8e2188c9fee219b708b45dc1f43
David Huggins-Daines <dhdaines@gmail.com>
2022-05-28 18:04:43 -0400

    refactor: change name of short_buf since it is not short and does not contain shorts

--- src/soundswallower-processor.js
+++ src/soundswallower-processor.js
@@ -3,11 +3,11 @@ class SoundSwallowerProcessor extends AudioWorkletProcessor {
     // too short) before sending back to the main thread
     constructor() {
 	super(...arguments);
-	this.short_buf = new Float32Array(2048);
+	this.buf = new Float32Array(2048);
 	this.pos = 0;
     }
     flush() {
-	const send_buf = this.short_buf.slice(0, this.pos);
+	const send_buf = this.buf.slice(0, this.pos);
 	this.pos = 0;
 	this.port.postMessage(send_buf, [send_buf.buffer]);
     }
@@ -23,7 +23,7 @@ class SoundSwallowerProcessor extends AudioWorkletProcessor {
 	    this.flush();
 	for (const i in input[0]) {
 	    const sample = input[0][i] + input[1][i];
-	    this.short_buf[this.pos++] = sample;
+	    this.buf[this.pos++] = sample;
 	}
 	// we don't actually produce output... but we want to live!
 	return true;
