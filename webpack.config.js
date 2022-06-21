6ab6ec4d213f52ecdbddbdcb46094f2ac0a2ddc0
David Huggins-Daines <dhdaines@gmail.com>
2022-06-21 19:24:34 -0400

    build: exclude unused files

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,7 @@ const config = {
 		{ from: modelDir,
 		  to: "model",
 		  globOptions: {
-		      ignore: ["**/fr-fr", "**/mdef.txt"],
+		      ignore: ["**/fr-fr", "**/mdef", "**/mdef.txt"],
 		  },
 		},
 	    ],
