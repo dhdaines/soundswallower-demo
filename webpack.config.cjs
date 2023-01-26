55be74042d0da58729c4f8c8ee5b1a6df5ff3188
David Huggins-Daines <dhdaines@gmail.com>
2023-01-26 13:33:45 -0500

    chore: small updates

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,5 +1,3 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
